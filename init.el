bbe9156bd7cad3d404d70a31fa3a79dfbdd590ee
Alan Severini <severini.alan@gmail.com>
2016-03-19 15:19:28 -0300

    Initial commit

---
+++ init.el
@@ -0,0 +1,415 @@
+(setq user-full-name "Alan Severini")
+(setq user-mail-address "severini.alan@gmail.com")
+
+(require 'cl)
+
+(load "package")
+(package-initialize)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+
+;;(setq package-archive-enable-alist '(("melpa" deft magit)))
+
+;; Y estos son mis paquetes
+(defvar alancho/packages '(autopair
+			   color-theme
+			   color-theme-tango
+			   auctex
+			   auto-complete
+			   ctable
+			   dash
+			   deferred
+			   deft
+			   elpy
+			   epc
+			   epl
+			   ess
+			   ;; esup
+			   exec-path-from-shell
+			   expand-region
+			   idle-highlight-mode
+			   ido-ubiquitous
+			   julia-mode
+			   magit
+			   paredit
+			   pkg-info
+			   popup
+			   request
+			   s
+			   smex
+			   websocket
+			   yaml-mode
+			   ;; yasnippet
+)
+  "Default packages")
+
+(defun alancho/packages-installed-p ()
+  (loop for pkg in alancho/packages
+        when (not (package-installed-p pkg)) do (return nil)
+        finally (return t)))
+
+(unless (alancho/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg alancho/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+;; Exec-path-from-shell
+;; ========================================================
+;; (require 'exec-path-from-shell) ;; if not using the ELPA package
+(exec-path-from-shell-initialize)
+
+;; Start in *scratch*
+;; ========================================================
+(setq inhibit-startup-message t)
+
+;; AucTeX
+;; ========================================================
+(add-hook 'LaTeX-mode-hook 'TeX-PDF-mode)
+
+(add-hook 'LaTeX-mode-hook '(lambda ()
+			      (if (string-match "\\.Rnw\\'" buffer-file-name)
+				  (setq fill-column 80))))
+
+(remove-hook 'text-mode-hook #'turn-on-auto-fill)
+
+(add-hook 'LaTeX-mode-hook 'turn-on-flyspell)
+
+;; To save and then run LaTeX in one command
+(defun my-run-latex ()
+  (interactive)
+  (TeX-save-document (TeX-master-file))
+  (TeX-command "LaTeX" 'TeX-master-file -1))
+
+(defun my-LaTeX-hook ()
+  (local-set-key (kbd "C-c C-c") 'my-run-latex))
+
+(add-hook 'LaTeX-mode-hook 'my-LaTeX-hook)
+
+;; Default directory
+;; ========================================================
+(setq default-directory "~/Dropbox/")
+
+;; Python
+;; ========================================================
+(elpy-enable)
+(elpy-use-ipython)
+(setq elpy-rpc-backend "jedi")
+(define-key elpy-mode-map [(shift return)] 'elpy-shell-send-region-or-buffer)
+(define-key elpy-mode-map [(C-return)] 'elpy-company-backend)
+
+;; Encryption
+;; ========================================================
+;; (require 'epa-file)
+;; (epa-file-enable)
+
+;; ESS
+;; ========================================================
+(require 'ess-site)
+
+(setq ess-ask-for-ess-directory nil)
+(setq ess-local-process-name "R")
+(setq ansi-color-for-comint-mode 'filter)
+(setq comint-scroll-to-bottom-on-input t)
+(setq comint-scroll-to-bottom-on-output t)
+(setq comint-move-point-for-output t)
+
+(defun my-ess-start-R ()
+  (interactive)
+  (if (not (member "*R*" (mapcar (function buffer-name) (buffer-list))))
+      (progn
+        (delete-other-windows)
+        (setq w1 (selected-window))
+        (setq w1name (buffer-name))
+        (setq w2 (split-window w1 nil t))
+        (R)
+        (set-window-buffer w2 "*R*")
+        (set-window-buffer w1 w1name))))
+
+(defun my-ess-eval ()
+  (interactive)
+  (my-ess-start-R)
+  (if (and transient-mark-mode mark-active)
+      (call-interactively 'ess-eval-region)
+    (call-interactively 'ess-eval-line-and-step)))
+
+(add-hook 'ess-mode-hook
+          '(lambda()
+             (local-set-key [(shift return)] 'my-ess-eval)))
+
+(add-hook 'inferior-ess-mode-hook
+          '(lambda()
+             (local-set-key [C-up] 'comint-previous-input)
+             (local-set-key [C-down] 'comint-next-input)))
+
+(add-hook 'Rnw-mode-hook
+          '(lambda()
+             (local-set-key [(shift return)] 'my-ess-eval)))
+
+;; This is to be quicker when writing the %>% operator from dplyr
+(defun then_R_operator ()
+  "R - %>% operator or 'then' pipe operator"
+  (interactive)
+  (just-one-space 1)
+  (insert "%>%")
+  (reindent-then-newline-and-indent))
+
+(define-key ess-mode-map (kbd "C-<return>") 'then_R_operator)
+
+(define-key inferior-ess-mode-map (kbd "C-<return>") 'then_R_operator)
+
+(defun then_ggplot_plus ()
+  "R - %>% operator or 'then' pipe operator"
+  (interactive)
+  (just-one-space 1)
+  (insert "+")
+  (reindent-then-newline-and-indent))
+
+(defun ess-roxygen-print-one-param (p)
+  "Insert parameter P to the roxygen comments."
+  (insert (format "##' @param %s\n" p)))
+
+(define-key ess-mode-map (kbd "C-+") 'then_ggplot_plus)
+
+(define-key inferior-ess-mode-map (kbd "C-+") 'then_ggplot_plus)
+
+;; RStudio indentation!
+(setq ess-default-style 'RStudio)
+
+(setq fill-column 72)
+(setq comment-auto-fill-only-comments t)
+(auto-fill-mode t)
+
+;; Define key-bindings for calling 'spin';
+;; 'M-n n' - spin the current buffer
+(define-key ess-mode-map "\M-nn" 'ess-swv-render)
+
+(defun ess-swv-render ()
+  "Run spin on the current .R file."
+  (interactive)
+  (ess-swv-run-in-R "rmarkdown::render"))
+
+;; Expand region
+;; ========================================================
+;; (require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+;; This are my faces, fonts, etcetera
+;; ========================================================
+(set-face-attribute 'default nil :height 120 :family "Inconsolata")
+
+
+;; This is to unfill paragraphs
+;; ========================================================
+(defun my-fill-latex-paragraph ()
+  "Fill the current paragraph, separating sentences w/ a newline.
+
+AUCTeX's latex.el reimplements the fill functions and is *very*
+convoluted. We use part of it --- skip comment par we are in."
+  (interactive)
+  (if (save-excursion
+        (beginning-of-line) (looking-at TeX-comment-start-regexp))
+      (TeX-comment-forward)
+    (let ((to (progn
+		(LaTeX-forward-paragraph)
+		(point)))
+	  (from (progn
+		  (LaTeX-backward-paragraph)
+		  (point)))
+	  (to-marker (make-marker)))
+      (set-marker to-marker to)
+      (while (< from (marker-position to-marker))
+	(forward-sentence)
+	(setq tmp-end (point))
+	(LaTeX-fill-region-as-paragraph from tmp-end)
+	(setq from (point))
+	(unless (bolp)
+	  (LaTeX-newline))))))
+
+(eval-after-load "latex"
+  '(define-key LaTeX-mode-map (kbd "M-q") 'my-fill-latex-paragraph))
+
+;; I don't need to highlight current line
+;; ========================================================
+(remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
+
+;; When C-x C-f I don't want to see this extensions
+;; ========================================================
+(custom-set-variables
+ '(completion-ignored-extensions (quote (".docx" ".xlsx" ".wmf" ".doc"
+					 ".xls" ".csv" ".bib" ".o" "~" ".bin" ".bak"
+					 ".obj" ".map" ".ico" ".pif" ".lnk" ".a" ".ln"
+					 ".blg" ".bbl" ".dll" ".drv" ".vxd" ".386" ".elc"
+					 ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/"
+					 ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class"
+					 ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".fasl" ".ufsl"
+					 ".fsl" ".dxl" ".pfsl" ".dfsl" ".p64fsl" ".d64fsl"
+					 ".dx64fsl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp"
+					 ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs"
+					 ".tps" ".vrs" ".pyc" ".pyo" ".odt" ".pptx" ".ppt" ".txt" ".dat"))))
+
+;; My magit setup
+;; ========================================================
+;; (require 'magit)
+;; (global-set-key (kbd "C-x g") 'magit-status)
+
+;; Recent mode
+;; ========================================================
+(recentf-mode t)
+;; 50 files ought to be enough.
+(setq recentf-max-saved-items 50)
+(global-set-key (kbd "C-x C-r") 'ido-recentf)
+
+(defun ido-recentf ()
+  "Use ido to select a recently opened file from the `recentf-list'"
+  (interactive)
+  (let
+      ((home (expand-file-name (getenv "HOME"))))
+    (find-file
+     (ido-completing-read
+      "Recentf open: "
+      (mapcar (lambda (path)
+                (replace-regexp-in-string home "~" path))
+              recentf-list)
+      nil t))))
+
+;; This is my color theme
+;; ========================================================
+(load-theme 'tango-dark)
+
+;; This is how I prefer text to behave
+;; ========================================================
+(delete-selection-mode 1)
+(setq shift-select-mode t)
+(transient-mark-mode t)
+(setq-default line-spacing 1)
+(setq auto-save-default nil)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(show-paren-mode 1)
+(electric-pair-mode 1)
+(visual-line-mode 1)
+(setq visible-bell t)
+(setq ispell-program-name "aspell")
+(prefer-coding-system 'utf-8)
+(setq column-number-mode t)
+;; (auto-fill-mode -1)
+
+(setq-default cursor-type 'bar)
+(blink-cursor-mode 1)
+(mapc
+ (lambda (face)
+   (set-face-attribute face nil :weight 'normal :underline nil))
+ (face-list))
+
+;; Show line number in the mode line.
+(line-number-mode 1)
+
+;; I've found that it's better and a best practice not to wrap lines
+;; when editing in LaTeX
+(set-default 'truncate-lines t)
+;; That said, the following must be deactivated
+;; (global-visual-line-mode 1)
+
+;; Display line numbers in margin
+(global-linum-mode 1)
+
+(defun unfill-region (beg end)
+  "Unfill the region, joining text paragraphs into a single
+    logical line.  This is useful, e.g., for use with
+    `visual-line-mode'."
+  (interactive "*r")
+  (let ((fill-column (point-max)))
+    (fill-region beg end)))
+
+;; Handy key definition
+(define-key global-map "\C-\M-Q" 'unfill-region)
+
+;; Smooth movement of buffer when scrolling or moving with arrow keys
+(setq redisplay-dont-pause t
+      scroll-margin 1
+      scroll-step 1
+      scroll-conservatively 10000
+      scroll-preserve-screen-position 1)
+
+;; Uniquify
+;; ========================================================
+;; (require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+;; Slightly more debatable
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; Yasnippets
+;; ========================================================
+;; (require 'yasnippet)
+;; (yas-global-mode 1)
+;; (global-set-key (kbd "C-.") 'yas/expand)
+
+;; I need smex
+;; ========================================================
+;; (require 'smex)
+;; (global-set-key (kbd "M-x") 'smex)
+;; (setq smex-save-file "~/.smex-items")
+;; (defun smex-update-after-load (unused)
+;;   (when (boundp 'smex-cache)
+;;     (smex-update)))
+;; (add-hook 'after-load-functions 'smex-update-after-load)
+
+(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+;; To answer quicker
+;; ========================================================
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; This is for dired mode to omit extensions I don't want to see
+(require 'dired-x)
+(setq-default dired-omit-files-p t) ; Buffer-local variable
+(setq dired-omit-files (concat dired-omit-files "\\|^\\..+$"))
+;; In dired, sort directories first
+(setq dired-listing-switches "-aBhl  --group-directories-first")
+
+;; Ignore case in eshell
+(setq eshell-cmpl-ignore-case t)
+
+;; Start emacs in eshell
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (cd default-directory)
+            (eshell)))
+
+;; I use to enable ido mode in org-mode. Because I don't use org
+;; anymore I need to define how to use ido next
+(ido-mode t)
+(setq ido-save-directory-list-file "~/.emacs.d/.ido.last")
+(setq ido-enable-flex-matching t)
+(setq ido-default-buffer-method 'selected-window)
+
+;; full screen
+(defun fullscreen ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen
+		       (if (frame-parameter nil 'fullscreen) nil 'fullboth)))
+(global-set-key [f11] 'fullscreen)
+
+;; Use the clipboard, pretty please, so that copy/paste "works"
+(setq x-select-enable-clipboard t)
+
+(require 'autopair)
+
+;; Para editar los yaml a ingresar en R scripts cuando render
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+
+;; Para ver las lineas vacias tambien
+(setq-default indicate-empty-lines t)
+(when (not indicate-empty-lines)
+  (toggle-indicate-empty-lines))
+
+;; Backup files no sirven para nada y nunca los he usado
+(setq make-backup-files nil)
