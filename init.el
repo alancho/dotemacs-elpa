ee9b6b7f041e3b93af5a36e0ac0f67c0cfd3b785
Alan Severini <severini.alan@gmail.com>
2016-05-24 20:31:43 -0300

    Add org repository

--- init.el
+++ init.el
@@ -8,6 +8,7 @@
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
 			 ("melpa-stable" . "http://stable.melpa.org/packages/")))
 
 ;; (add-to-list 'package-pinned-packages '(magit . "melpa-stable"))
