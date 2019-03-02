6ed300998cf9f4933b8f3a9727e44446404001aa
Alan Severini <alancho@debian>
2019-03-02 10:26:48 -0300

    Volver a usar linum-mode, desactivarlo en LaTeX

--- init.el
+++ init.el
@@ -120,6 +120,7 @@
 (add-hook 'TeX-mode-hook 'visual-line-mode)
 (add-hook 'TeX-mode-hook 'flyspell-mode)
 (add-hook 'TeX-mode-hook 'turn-on-reftex)
+(add-hook 'LaTeX-mode-hook (lambda () (linum-mode -1)))
 (add-hook 'LaTeX-mode-hook 'writeroom-mode)
 (setq font-latex-fontify-script nil)
 
@@ -432,7 +433,7 @@ convoluted. We use part of it --- skip comment par we are in."
 ;; (global-visual-line-mode 1)
 
 ;; Display line numbers in margin
-;; (global-linum-mode 1)
+(global-linum-mode 1)
 
 (defun unfill-region (beg end)
   "Unfill the region, joining text paragraphs into a single
