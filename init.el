b9d630450a71e0bbc8e4e771cbd34d1da5f20d67
Alan Severini <alancho@debian.INTA.AR>
2019-07-02 15:07:27 -0300

    Eliminar Seguir acá

--- init.el
+++ init.el
@@ -771,7 +771,7 @@ convoluted. We use part of it --- skip comment par we are in."
 (setq org-capture-templates '(("i" "Journal entry" entry (function org-journal-find-location)
                                "* %(format-time-string org-journal-time-format)%?")
 			      ("a" "Journal entry with link" entry (function org-journal-find-location)
-                               "* TODO Seguí acá: %(format-time-string org-journal-time-format)%?\n %a")))
+                               "* TODO %(format-time-string org-journal-time-format)%?\n %a")))
 
 (defun gtd ()
   (interactive)
