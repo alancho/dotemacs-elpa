e076bea4fde0dff8edefef26bfc574180da1aa06
Alan Severini <alancho@debian>
2020-10-06 15:44:29 -0300

    Ver eval en ess visibility

--- init.el
+++ init.el
@@ -199,6 +199,7 @@
 (setq comint-scroll-to-bottom-on-input t)
 (setq comint-scroll-to-bottom-on-output t)
 (setq comint-move-point-for-output t)
+(setq ess-eval-visibly-p t)
 
 (define-key
   ess-r-mode-map "_" #'ess-insert-assign)
