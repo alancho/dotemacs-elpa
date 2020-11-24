ffa5f7a109e48998903c0182f6abacc03684df37
Alan Severini <alancho@debian>
2020-11-24 09:24:27 -0300

    Disable marmalade repo que no anda

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 (package-initialize)
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ;; ("marmalade" . "http://marmalade-repo.org/packages/")
 			 ("melpa" . "http://melpa.org/packages/")
 			 ("org" . "http://orgmode.org/elpa/")
 			 ("melpa-stable" . "http://stable.melpa.org/packages/")))
